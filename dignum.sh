e102cb877b6cac9a57e2afe22035c3fd4f91e3d7
srushtimane27 <srushtimane31@gmail.com>
2022-06-19 11:51:14 +0530

    program files

---
+++ dignum.sh
@@ -0,0 +1,41 @@
+read -p " Enter a number: " a
+
+if [ $a -eq 1 ]
+then
+printf "One"
+
+elif [ $a -eq 2 ]
+then
+printf "Two"
+
+elif [ $a -eq 3 ]
+then
+printf "Three"
+
+elif [ $a -eq 4 ]
+then
+printf "Four"
+
+elif [ $a -eq 5 ]
+then
+printf "Five"
+
+elif [ $a -eq 6 ]
+then
+printf "Six"
+
+elif [ $a -eq 7 ]
+then
+printf "Seven"
+
+elif [ $a -eq 8 ]
+then
+printf "Eight"
+
+elif [ $a -eq 9 ]
+then
+printf "Nine"
+
+else
+printf "Number is greater than 9"
+fi
