ef0afe38d70e600e210f30318111bc8cb3e080b6
srushtimane27 <srushtimane31@gmail.com>
2022-06-19 12:05:14 +0530

    program files

---
+++ case1.sh
@@ -0,0 +1,23 @@
+read -p " Enter a number: " a
+case $a in
+1)
+echo "One"
+;;
+2)
+echo "Two"
+;;
+3)
+echo "Three"
+;;
+4)
+echo "Four"
+;;
+5)
+echo "Five"
+;;
+6)
+echo "Six"
+;;
+*)
+echo "Invalid Number"
+esac
